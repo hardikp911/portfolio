abaec2f7407a2de77046c8e701f7946de101edec
hardik3259 <parmar3259@gmail.com>
2024-02-08 17:05:30 +0530

    Update read.php

--- projects/read.php
+++ projects/read.php
@@ -1,28 +1,3 @@
 <?php
-require_once("./projects/function.php");
-// Set access control headers
-header("Access-Control-Allow-Origin: *"); // Change * to your specific domain if needed
-header("Access-Control-Allow-Methods: GET"); // Change GET to the methods you want to allow
-header("Content-Type: application/json");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, Access-Control-Allow-Headers, X-Request-With"); // Add the necessary headers
-
-
-$requestMethod = $_SERVER['REQUEST'];
-
-if($requestMethod == "GET"){
-    $projectList = projectList();
-    echo json_encode($projectList);
-
-    
-}else{
-    $data = array(
-        'status' => 405,
-        'message' => $requestMethod. "Method Not Allowed",
-    );
-    header("HTTP/1.0 405 Method Not Allowed");
-    echo json_encode($data);
-
-    
-}
-
+print_r ("hardik");
 ?>
