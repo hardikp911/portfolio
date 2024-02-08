d9255b757cdcbc246ecc91adda2b95480cc00fc7
hardik3259 <parmar3259@gmail.com>
2024-02-08 14:37:27 +0530

    update

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-header("Location: /main/");
-exit; 
+// Get the base URL
+$base_url = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
+
+// Redirect to the desired folder relative to the current location
+header("Location: $base_url/main/");
+exit; // Ensure that code execution stops after redirection
 ?>
