b999026622552f1d6d47201bc997471cd8fe5b66
hardik3259 <parmar3259@gmail.com>
2024-02-08 17:12:24 +0530

    changes made tocode

--- projects/read.php
+++ projects/read.php
@@ -9,23 +9,7 @@ header("Access-Control-Allow-Methods: GET"); // Change GET to the methods you wa
 header("Content-Type: application/json");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, Access-Control-Allow-Headers, X-Request-With"); // Add the necessary headers
 
-
-echo "jaaha";
-
-// Check if the 'REQUEST_METHOD' key exists in the $_SERVER superglobal array
 if(isset($_SERVER['REQUEST_METHOD'])) {
-    // Print the value of the 'REQUEST_METHOD' key
-    echo $_SERVER['REQUEST_METHOD'];
-} else {
-    // Handle the case where the 'REQUEST_METHOD' key does not exist
-    echo "REQUEST_METHOD is not defined";
-}
-
-die;
-
-$requestMethod = $_SERVER['REQUEST'];
-
-if($requestMethod == "GET"){
     $projectList = projectList();
     echo json_encode($projectList);
 
@@ -33,7 +17,7 @@ if($requestMethod == "GET"){
 }else{
     $data = array(
         'status' => 405,
-        'message' => $requestMethod. "Method Not Allowed",
+        'message' => $_SERVER['REQUEST_METHOD']. "Method Not Allowed",
     );
     header("HTTP/1.0 405 Method Not Allowed");
     echo json_encode($data);    
